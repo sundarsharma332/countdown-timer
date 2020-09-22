b510c8e152d027c182b35b2520084ea45ed2de84
Sundar Raj sharma <50244255+sundarsharma332@users.noreply.github.com>
2020-09-22 23:58:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "1 Jan 2021";
+const newYears = "3 Dec 2020";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
